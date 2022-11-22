d5a8c5c5cf11dcf3a93f8a31b39dd1cc9becb2e0
Stone Preston <stone.preston@canonical.com>
2022-11-22 16:50:20 -0600

    Add some basic controller logic

--- controllers/jujuconfig_controller.go
+++ controllers/jujuconfig_controller.go
@@ -18,13 +18,18 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
+	bootstrapv1beta1 "github.com/charmed-kubernetes/cluster-api-bootstrap-provider-juju/api/v1beta1"
+	"github.com/pkg/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog/v2"
+	bsutil "sigs.k8s.io/cluster-api/bootstrap/util"
+	"sigs.k8s.io/cluster-api/util"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	bootstrapv1beta1 "github.com/charmed-kubernetes/cluster-api-bootstrap-provider-juju/api/v1beta1"
 )
 
 // JujuConfigReconciler reconciles a JujuConfig object
@@ -36,6 +41,8 @@ type JujuConfigReconciler struct {
 //+kubebuilder:rbac:groups=bootstrap.bootstrap.cluster.x-k8s.io,resources=jujuconfigs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=bootstrap.bootstrap.cluster.x-k8s.io,resources=jujuconfigs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=bootstrap.bootstrap.cluster.x-k8s.io,resources=jujuconfigs/finalizers,verbs=update
+//+kubebuilder:rbac:groups=cluster.x-k8s.io,resources=clusters;clusters/status;machinesets;machines;machines/status;machinepools;machinepools/status,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=secrets;events;configmaps,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,9 +54,56 @@ type JujuConfigReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *JujuConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// Lookup the juju config
+	config := &bootstrapv1beta1.JujuConfig{}
+	if err := r.Client.Get(ctx, req.NamespacedName, config); err != nil {
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Failed to get config")
+		return ctrl.Result{}, err
+	}
+	log.Info("Retrieved JujuConfig successfully")
+
+	configOwner, err := bsutil.GetConfigOwner(ctx, r.Client, config)
+	if apierrors.IsNotFound(err) {
+		// Could not find the owner yet, this is not an error and will rereconcile when the owner gets set.
+		log.Info("Config Owner could not be found yet ...")
+		return ctrl.Result{}, nil
+	}
+	if err != nil {
+		log.Error(err, "Failed to get Owner Config")
+		return ctrl.Result{}, err
+	}
+	if configOwner == nil {
+		log.Info("Config Owner was nil ...")
+		return ctrl.Result{}, nil
+	}
+	log.Info("Retrieved config owner successfully")
+
+	log = log.WithValues("kind", configOwner.GetKind(), "version", configOwner.GetResourceVersion(), "name", configOwner.GetName())
+
+	log = log.WithValues("Cluster", klog.KRef(configOwner.GetNamespace(), configOwner.ClusterName()))
+	ctx = ctrl.LoggerInto(ctx, log)
+
+	// Lookup the cluster the config owner is associated with
+	cluster, err := util.GetClusterByName(ctx, r.Client, configOwner.GetNamespace(), configOwner.ClusterName())
+	if err != nil {
+		if errors.Cause(err) == util.ErrNoCluster {
+			log.Info(fmt.Sprintf("%s does not belong to a cluster yet, waiting until it's part of a cluster", configOwner.GetKind()))
+			return ctrl.Result{}, nil
+		}
+
+		if apierrors.IsNotFound(err) {
+			log.Info("Cluster does not exist yet, waiting until it is created")
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Could not get cluster with metadata")
+		return ctrl.Result{}, err
+	}
+	log.Info("Retrieved cluster successfully", "cluster", cluster.ObjectMeta)
 
 	return ctrl.Result{}, nil
 }
