0c387f21b6e052304580218e49f3c6a89799b977
George Kraft <george.kraft@canonical.com>
2023-04-11 16:09:22 -0500

    Fix conflict installing docker package

--- test/integration/setup-env.sh
+++ test/integration/setup-env.sh
@@ -2,12 +2,10 @@
 set -euxo pipefail
 
 # This script sets up an environment for running integration tests with a local
-# microk8s cluster.
+# microk8s cluster. Assumes that make, gcc/clang, and docker are already installed
 
-sudo apt-get update
-sudo apt-get install -y make gcc docker.io
 sudo snap install microk8s --classic
 sudo snap install go --channel 1.19/stable --classic
 sudo snap install kubectl --classic
 sudo microk8s enable registry
-sudo usermod -aG docker,microk8s ubuntu
+sudo usermod -aG microk8s ubuntu
